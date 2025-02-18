6978b1c835eb42ebea06742e843a7a519baab2b2
MAZOUNI Mohamed Rayan <88382756+mazounirayan@users.noreply.github.com>
2025-02-18 21:02:47 +0100

    Update FamilleDTO.kt

--- app/src/main/java/com/example/familyapp/network/dto/familleDto/FamilleDTO.kt
+++ app/src/main/java/com/example/familyapp/network/dto/familleDto/FamilleDTO.kt
@@ -1,8 +1,2 @@
 package com.example.familyapp.network.dto.familleDto
 
-data class FamilleDTO(
-    val idFamille: Int,
-    val nom: String,
-    val date_de_creation: String,
-    val code_invitation: String
-)
